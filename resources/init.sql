f787d3169380a21a9aba7553631eb56e00ddcd07
Jay Ryan Jamorabon <43054912+jayryanj@users.noreply.github.com>
2025-02-28 15:20:44 -0800

    Modified license table

--- resources/init.sql
+++ resources/init.sql
@@ -1,5 +1,10 @@
+CREATE TYPE license_type AS ENUM ('perpetual', 'timed', 'trial');
+
 CREATE TABLE IF NOT EXISTS license(
     license_key VARCHAR(20) PRIMARY KEY,
-    expiration TIMESTAMP
+    product TEXT,
+    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    expiration TIMESTAMPTZ,
+    type license_type NOT NULL,
+    description TEXT
 );
-
